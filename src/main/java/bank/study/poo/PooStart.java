049407e188181d20ffeea1ee7a04e522ba0e6967
Antonio <rodrigowserra@gmail.com>
2024-01-19 20:57:37 +0000

    teste

--- src/main/java/bank/study/poo/PooStart.java
+++ src/main/java/bank/study/poo/PooStart.java
@@ -4,7 +4,7 @@ public class PooStart {
 
 	public static void main(String[] args) {
 		Pessoa pessoa1 = new PessoaFisica();
-		Pessoa pessoa2 = new PessoaJuridica();
+		Pessoa pessoa2 = new PessoaJuridica(); 
 		
 		
 
