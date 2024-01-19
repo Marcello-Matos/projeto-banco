56e4fcfc0822895660151e401c91cf766aeaf3ff
Antonio <rodrigowserra@gmail.com>
2024-01-19 20:29:35 +0000

    Start Project

---
+++ src/main/java/bank/study/poo/PooStart.java
@@ -0,0 +1,13 @@
+package bank.study.poo;
+
+public class PooStart {
+
+	public static void main(String[] args) {
+		Pessoa p = new Pessoa("Marcello", 20);
+		
+		System.out.println(p.getNome());
+		System.out.println(p.getIdade());
+
+	}
+
+}
